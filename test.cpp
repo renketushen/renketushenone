0a7586bf80e691047e8ef82896a469a01c6be28c
renketushen <904103792@qq.com>
2016-01-26 20:02:16 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,7 +2,8 @@
 public Value get(Key key) {
     return get(root, key);
 }
-
+//第一次修改
+//测试
 private Value get(Node x, Key key) {
     if (x == null) return null;
     int cmp = key.compareTo(x.key);
